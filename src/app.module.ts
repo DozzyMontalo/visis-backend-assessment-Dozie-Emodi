f7d073dae751f84582772f76e6f3c4ec198ada09
DozzyMontalo <dozienice@gmail.com>
2024-07-28 23:04:17 +0100

    setting up and creating modules

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { BookModule } from './book/book.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BookModule
+  ],
+  
+})
+export class AppModule {}
